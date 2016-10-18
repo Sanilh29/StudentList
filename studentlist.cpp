ce5a0cce1c6414cd85fa62fbe201018d7a3eecd9
U-BSD\456570 <456570@SHS-10D8A6AA9.beaverton.k12.or.us>
2016-10-18 14:24:30 -0700

    created methods

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,9 +1,45 @@
 #include <cstring>
 #include <iostream>
 
+using namespace std;
+
+void addStudent();
+void printStudent();
+void deleteStudent();
+
 struct Student {
   float gpa;
   int id;
-  char firstname;
-  char lastname;
+  char* firstname;
+  char* lastname;
+};
+
+int main() {
+  char command[10];
+  cout << "Enter of the commands listed: add, remove, print." << endl;
+  cin >> command;
+  if (0 == strcmp(command, "add")){
+    addStudent();
+    }
+  if (0==strcmp(command, "remove")){
+    printStudent();
+    }
+  if (0==strcmp(command, "print")){
+    deleteStudent();
+  }
+  return 0;
+}
+
+void addStudent(){
+  cout <<"hi" << endl;
+}
+
+
+void printStudent(){
+  cout << "hi" << endl;
+}
+
+
+void deleteStudent(){
+  cout << "hi" << endl;
 }
