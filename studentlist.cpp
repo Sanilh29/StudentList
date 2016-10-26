9c993376bffd8d520b0c4a38be8a61409c68970e
U-BSD\456570 <456570@SHS-10D8A6AA9.beaverton.k12.or.us>
2016-10-26 14:29:05 -0700

    worked on add

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,11 +1,8 @@
 #include <cstring>
+#include <vector>
 #include <iostream>
 
 using namespace std;
-
-void addStudent();
-void printStudent();
-void deleteStudent();
 
 struct Student {
   float gpa;
@@ -14,32 +11,50 @@ struct Student {
   char* lastname;
 };
 
+
+void addStudent(vector<Student*>*list);
+void printStudent(vector<Student*>*list);
+void deleteStudent(vector<Student*>*list);
+
+
 int main() {
   char command[10];
+  vector<Student*>*list = new vector<Student*>();
   cout << "Enter of the commands listed: add, remove, print." << endl;
   cin >> command;
   if (0 == strcmp(command, "add")){
-    addStudent();
+    addStudent(list);
     }
   if (0==strcmp(command, "remove")){
-    printStudent();
+    printStudent(list);
     }
   if (0==strcmp(command, "print")){
-    deleteStudent();
+    deleteStudent(list);
   }
   return 0;
 }
 
-void addStudent(){
-  cout <<"hi" << endl;
+void addStudent(vector<Student*>*list){
+  Student* student = new Student;
+  int newid;
+  char newfirstname[20];
+  char newlastname[20];
+  cout << "What's your first name?" << endl;
+  cin.get(newfirstname, 20);
+  cin.ignore();
+  cout << "What's your last name?" << endl;
+  cin.get(newlastname,20);
+  cin.ignore();
+  
 }
 
 
-void printStudent(){
+void printStudent(vector<Student*>*list){
   cout << "hi" << endl;
+ 
 }
 
 
-void deleteStudent(){
+void deleteStudent(vector<Student*>*list){
   cout << "hi" << endl;
 }
