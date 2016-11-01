92ee2caa42bf4e6b295c730276d133963d60c621
U-BSD\456570 <456570@SHS-10D8A6AA9.beaverton.k12.or.us>
2016-11-01 14:08:32 -0700

    finished delete and commented

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,10 +1,13 @@
+//Sanil Hattangadi, 11/1/16, keeps a directory of students with their first and last name, id, and GPA; can add, delete and print students
+
 #include <cstring>
+#include <iomanip> //imports setprecison
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-struct Student {
+struct Student { //general variables that I will use 
   float gpa;
   int id;
   char* firstname;
@@ -12,35 +15,35 @@ struct Student {
 };
 
 
-void addStudent(vector<Student*>*list);
+void addStudent(vector<Student*>*list); //initizialises all the methods
 void printStudent(vector<Student*>*list);
 void deleteStudent(vector<Student*>*list);
 
 
 int main() {
   char command[10];
-  vector<Student*>*list = new vector<Student*>();
+  vector<Student*>*list = new vector<Student*>();//vector size can change to whatever
   bool playing = true;
   while (playing == true){
-    cout << "Enter of the commands listed: Add, Remove, Print, or Quit." << endl;
+    cout << "Enter of the commands listed: add, delete, print, or quit." << endl;
     cin >> command;
-    if (0 == strcmp(command, "Add")){
+    if (0 == strcmp(command, "add")){
       addStudent(list);
     }
-    if (0==strcmp(command, "Remove")){
+    if (0==strcmp(command, "delete")){
       deleteStudent(list);
     }
-    if (0==strcmp(command, "Print")){
+    if (0==strcmp(command, "print")){
       printStudent(list);
     }
-    if (0==strcmp(command, "Quit")){
+    if (0==strcmp(command, "quit")){
       playing = false;
-    } 
+    }
   }
 }
 void addStudent(vector<Student*>*list){
-  Student* student = new Student;
-  student->firstname = new char[25];
+  Student* student = new Student;//adds student to the heap
+  student->firstname = new char[25];//the char inputted will place the first name into firstname student
   student->lastname = new char[25];
   cout << "What's your first name?" << endl;
   cin.ignore();
@@ -50,26 +53,37 @@ void addStudent(vector<Student*>*list){
   cin.get(student->lastname,25);
   cin.ignore();
   cout << "What's your student ID?" << endl;
-  cin.ignore();
   cin >> student->id;
   cin.ignore();
   cout << "What's your GPA?"  << endl;
-  cin.ignore();
   cin >> student->gpa;
   cin.ignore();
-  list->push_back(student);
+  list->push_back(student); //pushes student into the vector
 }
 
 
 void printStudent(vector<Student*>*list){
-  for (vector<Student*>::iterator it=(*list).begin(); it !=(*list).end(); ++it){
-    cout << (*(*it)).firstname << " " << (*(*it)).lastname << endl ;
-    //cout << (*it).-> firstname << endl;
+  for (vector<Student*>::iterator it=(*list).begin(); it !=(*list).end(); ++it){//go through each iterator
+    cout << "Name: " << (*(*it)).firstname << " " << (*(*it)).lastname << endl ;//print first and last name
+    cout << "Student ID: " << (*it)-> id << endl;// print ID
+    cout << "GPA: " << setprecision(2) << fixed << (*it)-> gpa << endl; //print GPA to 2 decimal places each time
   }
 }
 
 
 void deleteStudent(vector<Student*>*list){
-  cout << "hi" << endl;
+  int deleteid;
+  cout << "Enter the Student ID you wish to delete." << endl; 
+  cin >> deleteid;
+  cout << deleteid << endl;
+  cout << "Student Deleted" << endl;
+  for (vector<Student*>::iterator it=(*list).begin(); it != (*list).end(); ++it){ //go through the iterator
+    if ((*it)->id == deleteid){//if the iterator is on the deleted ID
+      cout << deleteid << endl;
+      delete (*it); //delete student
+      (*list).erase(it); //delete pointer
+      break;//quit the forloop
+    }
+  }
 }
 
